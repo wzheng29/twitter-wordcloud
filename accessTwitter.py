d026f96ddc623ce2de9a74c37e9772e914368085
wzheng29 <wzheng29@buffalo.edu>
2021-05-22 19:53:32 -0400

    Use k-means clustering to group common words together

--- accessTwitter.py
+++ accessTwitter.py
@@ -48,6 +48,22 @@ def processTweets():
 
     return group_words
 
+def tokenizer(keyword):
+    #stemmer = PorterStemmer()
+    return [word for word in keyword.split(' ')]
+
 if __name__ == "__main__":
-    print(processTweets())
-    
+    keywords = processTweets()
+    tfidf = TfidfVectorizer(tokenizer=tokenizer, stop_words=stopwords.words('english'))
+    X = pd.DataFrame(tfidf.fit_transform(keywords).toarray(), index=keywords, columns=tfidf.get_feature_names())
+    features = tfidf.fit_transform(keywords)
+    k = 5
+    model = KMeans(n_clusters=k, init='k-means++', n_init=2, max_iter=100)
+    model.fit(features)
+    order_centroids = model.cluster_centers_.argsort()[:,::-1]
+    terms = tfidf.get_feature_names()
+    for i in range(k):
+        print("cluster ID %d: " % i)
+        for j in order_centroids[i, :10]:
+            print(' %s' % terms[j])
+        print('--------------------------------')
